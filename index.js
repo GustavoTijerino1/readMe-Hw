6145702e8cf4205af2b1ae95547b78620295ba35
Gustavo Tijerino <busg26@gmail.com>
2022-03-25 23:58:11 -0500

    Updated Index.js

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-var ti;
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project name?',
+      name: 'projectName',
+    },
+    {
+      type: 'input',
+      message: 'What are the steps required to install your project?',
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: 'Describe how to use this program',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'Would you like to add a license?',
+      choices: [
+        'MIT License',
+        'Mozilla Public License 2.0',
+      ],
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: "Enter your GitHub username."  ,  
+      name: 'username'
+    },
+    {
+      type: 'input',
+      message: "Enter your email address.",
+      name: 'email'
+    }  
+    
+  
+  ])
+  .then((response) => {
+      response.usage
+    fs.writeFile('README_2.0.md',
+    `
+# ${response.projectName}
+
+## Description
+
+This project lets you generate a professional README.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project?
+
+${response.install}
+## Usage
+
+${response.usage}
+Provide instructions and examples for use. Include screenshots as needed.
+
+
+## License
+${response.license}
+
+## Contact me
+[GitHub](https://github.com/${response.username})
+
+Email:  ${response.email}
+
+
+---
+
+    
+    
+    `, (err) =>
+    err ? console.error(err) : console.log('README file created')
+    );
+    });
